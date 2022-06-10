1b68ae67ea579650a02f840a053f33248050c8cd
Nithin <78612244+nithinrdy@users.noreply.github.com>
2022-06-10 20:11:43 +0530

    add disease model

--- src/app/models/disease.ts
+++ src/app/models/disease.ts
@@ -1,3 +1,10 @@
 export interface Disease {
-  
+  cleaned_disease: string;
+  disease: string;
+  primary_description: string;
+  rarity: string | null;
+  raw_symptoms: string | null;
+  secondary_description: string | null;
+  subtitle: string | null;
+  symptom_possibility: string | null;
 }
