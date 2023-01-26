dc39dfcfe402dbfa60acb2e00ae852559797fef2
Nithin <78612244+nithinrdy@users.noreply.github.com>
2023-01-27 00:30:54 +0530

    use formGroups instead

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,8 +14,22 @@ import { DitemComponent } from './components/ditem/ditem.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
-  declarations: [AppComponent, LrPageComponent, HomeComponent, SearchComponent, QueryComponent, DitemComponent, ProfileComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    LrPageComponent,
+    HomeComponent,
+    SearchComponent,
+    QueryComponent,
+    DitemComponent,
+    ProfileComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/lr-page/lr-page.component.ts
+++ src/app/components/lr-page/lr-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService, User } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-lr-page',
@@ -9,16 +10,49 @@ import { Router } from '@angular/router';
 export class LrPageComponent implements OnInit {
   constructor(private userService: UserService, private router: Router) {}
 
+  USERNAME_MIN_LENGTH = 6;
+  USERNAME_MAX_LENGTH = 12;
+  PASSWORD_MIN_LENGTH = 8;
+  PASSWORD_MAX_LENGTH = 20;
+
+  loginFormGroup = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(this.USERNAME_MIN_LENGTH),
+      Validators.maxLength(this.USERNAME_MAX_LENGTH),
+      Validators.pattern('^[A-Za-z][A-Za-z0-9]*'),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(this.PASSWORD_MIN_LENGTH),
+      Validators.maxLength(this.PASSWORD_MAX_LENGTH),
+    ]),
+  });
+  registerFormGroup = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(this.USERNAME_MIN_LENGTH),
+      Validators.maxLength(this.USERNAME_MAX_LENGTH),
+      Validators.pattern('^[A-Za-z][A-Za-z0-9]*'),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(this.PASSWORD_MIN_LENGTH),
+      Validators.maxLength(this.PASSWORD_MAX_LENGTH),
+    ]),
+  });
+
   ngOnInit(): void {}
 
+  getObjectKeys(object: object): string[] {
+    return Object.keys(object);
+  }
+
   login(): void {
-    let usernameInput = document.querySelector(
-      '.login-username'
-    ) as HTMLInputElement;
-    let passwordInput = document.querySelector(
-      '.login-password'
-    ) as HTMLInputElement;
-    this.userService.login(usernameInput.value, passwordInput.value);
+    this.userService.login(
+      this.loginFormGroup.controls['username'].value,
+      this.loginFormGroup.controls['password'].value
+    );
   }
 
   get user(): User | null {
@@ -26,13 +60,10 @@ export class LrPageComponent implements OnInit {
   }
 
   register(): void {
-    let usernameInput = document.querySelector(
-      '.register-username'
-    ) as HTMLInputElement;
-    let passwordInput = document.querySelector(
-      '.register-password'
-    ) as HTMLInputElement;
-    this.userService.register(usernameInput.value, passwordInput.value);
+    this.userService.register(
+      this.registerFormGroup.controls['username'].value,
+      this.registerFormGroup.controls['password'].value
+    );
   }
 
   logout(): void {
